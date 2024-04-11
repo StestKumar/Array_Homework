4c71c472d6095010860cec3398d57f2e792a801a
Prashant Kumar <softwaretesterKumar@gmail.com>
2024-04-11 22:14:52 +0100

    Updated Homework

--- src/Armstrong_Number.java
+++ src/Armstrong_Number.java
@@ -1,31 +1,44 @@
-//WAP to input any number and find out if its Armstrong number or not?
-
 import java.util.Scanner;
 
 public class Armstrong_Number {
     public static void main(String[] args) {
-        // Declare the variables to be used
-        int Number;
-        int Rem;
-        int Armstrong = 0;
 
-        System.out.println("Please enter any number :");
         Scanner scanner = new Scanner(System.in);
-        Number = scanner.nextInt();
+        System.out.println("Please enter any number :");
+        int Number = scanner.nextInt();
 
-        int x = Number;
+        int A1 = Number;
+        int length = 0;
 
-        while (Number > 0) {
-            Rem = Number % 10;
-            Armstrong = (Rem * Rem * Rem) + Armstrong;
-            Number = Number / 10;
-
-        }
-        if (x == Armstrong) // both the variable should be equal
-            System.out.println("Number entered is a Armstrong number");
-        else {
-            System.out.println("Number entered is not a Armstrong number");
+        while (A1 != 0)
+        {
+            length = length + 1;
+            A1 = A1 / 10;
         }
 
+        int A2 = Number;
+        int Armstrong = 0;
+        int Remainder;
+        while (A2 != 0)
+        {
+            int Multiply = 1;
+            Remainder = A2 % 10;
+            for (int i = 1; i <= length; i++)
+            {
+                Multiply = Multiply * Remainder;
+            }
+
+            Armstrong = Armstrong + Multiply;
+            A2 = A2 / 10;
+
+        }
+        if (Armstrong == Number) {
+            System.out.println("Armstrong Number");
+        } else {
+            System.out.println("Not Armstrong Number");
+
+        }
     }
+
 }
+
